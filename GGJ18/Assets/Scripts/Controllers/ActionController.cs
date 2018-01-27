32210f0b845afa21dba04a0e633cf9bfb2930375
BStockwellDev <brandon.g.stockwell@gmail.com>
2018-01-27 00:52:50 -0500

    Added template scripts for objects and controllers
    Added template scripts for objects and controllers. Created Object hierarchy and controllers for checking game states and causing transitions to occur under certain conditions (moving rooms, etc.)

---
+++ GGJ18/Assets/Scripts/Controllers/ActionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour {
+
+	/* Controller methods:
+	 * - Checks conditions every frame for win or lose, communicates state when triggered to GameStateController
+	 * - Tracks PlayerObject throughout level every frame for change in conditions: interacting with a HackableObject
+	*/
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GGJ18/Assets/Scripts/Controllers/GameStateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateController : MonoBehaviour {
+
+	/* Controller methods:
+	 * - monitor and check game state based on user input and check on a given frame frequency
+	 * - Lose State: run into danger obstacle, receive lose condition from action controller and restart game
+	 * - Ongoing State: Nothing occurs, checks for action controller input
+	 * - Win State: reach end of level point, receive win condition from action controller
+	*/
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
