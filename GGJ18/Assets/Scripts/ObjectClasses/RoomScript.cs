da28984840b0ec506b10a94c9bcddb148f087dc3
squire88 <squire88@users.noreply.github.com>
2018-01-27 18:35:28 -0500

    camera stuff

---
+++ GGJ18/Assets/Scripts/ObjectClasses/RoomScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Collision2D Player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Collider2D>();
+
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		
+		if (coll.gameObject.tag == "Player") {
+			Collider2D self = GetComponent<Collider2D> ();
+			Camera.main.pixelRect = GetComponent<RectTransform> ().rect;
+	
+
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D coll) {
+		
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+
+
+		if (coll.gameObject.tag == "Player") {
+			
+		}
+	}
+
+
+		
+}
