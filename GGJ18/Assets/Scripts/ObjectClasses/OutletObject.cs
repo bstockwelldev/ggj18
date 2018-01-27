80e32bbca9db6a22b75c00cf5f6b382bf93ba1c2
BStockwellDev <brandon.g.stockwell@gmail.com>
2018-01-27 12:12:09 -0500

    Modified outlet object to use trigger collider
    Displays debug message in console when an object enters the trigger collider for the outlet sprite.

---
+++ GGJ18/Assets/Scripts/ObjectClasses/OutletObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutletObject : HackableObject {
+
+	/* Object methods:
+	 * - responding to dynamic object interaction with a given function
+	 * - collision interactions (basic) -- transport user from one location to another on level
+	 * - input value from user when the player object is colliding with the outlet or door
+	 * - node connections between outlets and doors of a level
+	*/
+
+//	Collision2D collider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// check for collision with player
+//		OnCollisionEnter2D(collider);
+	}
+
+//	void OnCollisionEnter2D(Collision2D coll) {
+//		if (coll.gameObject.tag == "Player")
+//			coll.gameObject.SendMessage("Transmit", "ActOnOutlet");
+//
+//	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Object Entered the trigger");
+	}
+
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("Object is within the trigger");
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("Object Exited the trigger");
+	}
+}
